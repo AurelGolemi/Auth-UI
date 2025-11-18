a93150af14c9f2a9b93b02c8fbe7cfce6973fe75
Aurel Golemi <golemiaurel68@gmail.com>
2025-11-18 22:43:55 +0200

    Changed File Structure. Implementing Login Form Page.

--- app/[auth]/login/page.tsx
+++ app/[auth]/login/page.tsx
@@ -1,1 +1,8 @@
 'use client'
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { Lock, Github, Chrome } from 'lucide-react';
+import { signIn } from 'next-auth/react';
+import { LoginForm } from '@/app/components/auth/login-form';
+import { RegisterForm } from '@/app/components/auth/register-form';
